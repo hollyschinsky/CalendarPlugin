a180c5c3d9cb4d72609861daefa5b51a59730cbd
Holly Schinsky <hollyschinsky@gmail.com>
2013-09-09 14:27:45 -0400

    Initial checkin

---
+++ www/calendar.js
@@ -0,0 +1,17 @@
+var calendar = {
+    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'Calendar', // mapped to our native Java class called "CalendarPlugin"
+            'addCalendarEntry', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "title": title,
+                "description": notes,
+                "eventLocation": location,
+                "startTimeMillis": startDate.getTime(),
+                "endTimeMillis": endDate.getTime()
+            }]
+        ); 
+    }
+}
